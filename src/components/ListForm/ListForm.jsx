72e799d7f8bd15a98af73d144e9b66845e215633
Rodrigo Ponce <rodrigo.ponce@tsoftglobal.com>
2023-12-04 11:12:44 -0300

    created, form, validations, and modal for product list

--- src/components/ListForm/ListForm.jsx
+++ src/components/ListForm/ListForm.jsx
@@ -1,46 +1,167 @@
 import { useState } from 'react'
-import { View } from 'react-native'
-import { Input, Button } from "@rneui/themed"
+import 'react-native-get-random-values';
+import { v4 as uuidv4 } from 'uuid';
+import { View, ScrollView } from 'react-native'
+import { Input, Button, Text, ListItem } from "@rneui/themed"
+import { useFormik } from 'formik'
+import { initialValues, validationSchema } from './ListForm.data'
+import { Modal } from '../Modal/Modal'
 import { styles } from "./ListForm.styles"
 
 export function ListForm() {
 
-    const [ productList, setProductList] = useState([])
+    const [productList, setProductList] = useState([])
+    const [totalPrice, setTotalPrice] = useState(0)
+    const [showModal, setShowModal] = useState(false)
 
+    const additionProductListPrice = (updatedProductList) => {
+        const productPrices = updatedProductList.map(product => product.price)
+        const totalPriceReducer = productPrices.reduce((total, price) => total + price, 0);
+        setTotalPrice(totalPriceReducer)
+    }
+
+
+    const formik = useFormik({
+        initialValues: initialValues(),
+        validationSchema: validationSchema(),
+        validateOnChange: false,
+        onSubmit: (formValues, { resetForm }) => {
+            const stringToNumber = parseFloat(formValues.price)
+
+            const newProduct = {
+                id: uuidv4(),
+                productName: formValues.productName,
+                price: stringToNumber
+            }
+            setProductList(prevProductList => {
+                const updatedProductList = [...prevProductList]
+                updatedProductList.unshift(newProduct)
+                additionProductListPrice(updatedProductList)
+                return updatedProductList
+            })
+
+            resetForm({ values: { ...initialValues } })
+        }
+    })
+
+    const OpenCloseModal = () => {
+        setShowModal(prevState => !prevState)
+    }
+
+    const deleteProducList = () => {
+        setProductList([])
+        setTotalPrice(0)
+        OpenCloseModal()
+    }
+
+    const removeProductFromList = (id) => {
+        const filteredList = productList.filter((product) => {
+            return product.id !== id
+        })
+        setProductList(filteredList)
+        additionProductListPrice(filteredList)
+    }
 
     return (
-        <View style={styles.inputsContainer}>
-            <Input
-                containerStyle={{ width: '55%' }}
-                placeholder='Producto'
-                leftIcon={{
-                    name: 'basket-plus',
-                    type: 'material-community',
-                    size: 24,
-                    color: '#848484',
-                }}
-            />
-            <Input
-                containerStyle={{ width: '30%' }}
-                leftIcon={{
-                    name: 'currency-usd',
-                    type: 'material-community',
-                    size: 24,
-                    color: '#848484',
-                    paddingBottom: 0
-                }} 
-            />
+        <View>
+            <View style={styles.inputsContainer}>
+                <Input
+                    containerStyle={{ width: "48%" }}
+                    placeholder='Producto'
+                    value={formik.values.productName}
+                    leftIcon={{
+                        name: 'basket-plus',
+                        type: 'material-community',
+                        size: 24,
+                        color: '#848484',
+                    }}
+                    errorMessage={formik.errors.productName}
+                    onChangeText={(text) => formik.setFieldValue('productName', text)}
+                />
+                <Input
+                    containerStyle={{ width: '35%' }}
+                    placeholder='Precio'
+                    value={formik.values.price}
+                    leftIcon={{
+                        name: 'currency-usd',
+                        type: 'material-community',
+                        size: 24,
+                        color: '#848484',
+                        paddingBottom: 0
+                    }}
+                    errorMessage={formik.errors.price}
+                    onChangeText={(price) => formik.setFieldValue('price', price)}
+                />
 
-            <Button
-                buttonStyle={styles.button}
-                containerStyle={{ marginBottom: 20 }}
-                icon={{
-                    name: 'plus',
-                    type: 'material-community',
-                    size: 20,
-                    color: 'white',
-                }}
-            />
+                <Button
+                    buttonStyle={styles.button}
+                    containerStyle={{ marginBottom: 20 }}
+                    icon={{
+                        name: 'plus',
+                        type: 'material-community',
+                        size: 20,
+                        color: 'white',
+                    }}
+                    onPress={formik.handleSubmit}
+                    onLoading={formik.isSubmitting}
+                />
+            </View>
+
+            <View style={styles.totalContainer}>
+                {
+                    productList.length > 0 && (
+                        <>
+                            <Button title='Borrar lista' buttonStyle={styles.btnDeleteList} onPress={OpenCloseModal} />
+                            <Text style={styles.totalText}>Total  ${totalPrice}</Text>
+                        </>
+                    )
+                }
+            </View>
+
+            <ScrollView>
+                {
+                    productList.map((product, i) => (
+                        <ListItem.Swipeable
+                            key={i}
+                            leftWidth={60}
+                            rightWidth={40}
+                            minSlideWidth={40}
+                            leftContent={() => (
+                                <Button
+                                    containerStyle={{
+                                        flex: 1,
+                                        justifyContent: "center",
+                                        backgroundColor: "red",
+                                        alignItems: 'start',
+                                    }}
+                                    type="clear"
+                                    icon={{
+                                        name: "delete-outline",
+                                        type: "material-community",
+                                        size: 28,
+                                        color: '#ffffff',
+                                    }}
+                                    onPress={() => removeProductFromList(product.id)}
+                                />
+                            )}
+                        >
+                            <ListItem.Content>
+                                <ListItem.Title style={{ fontSize: 20 }}>{product.productName}</ListItem.Title>
+                            </ListItem.Content>
+                            <ListItem.Title style={{ fontSize: 18, fontWeight: 'bold' }}>$ {product.price}</ListItem.Title>
+                            <ListItem.Chevron />
+                        </ListItem.Swipeable>
+                    ))
+                }
+            </ScrollView>
+
+            <Modal show={showModal}>
+                <Text style={{ textAlign: 'center', fontSize: 18, marginBottom: 16 }}>Desea borrar la lista completa?</Text>
+                <View style={styles.confirmModal}>
+                    <Button title="Borrar lista" onPress={deleteProducList} buttonStyle={styles.deleteBtn} />
+                    <Button title='Cancelar' buttonStyle={styles.cancelBtn} onPress={OpenCloseModal} />
+                </View>
+            </Modal>
         </View>
     )
 }
--- src/components/ListForm/ListForm.styles.js
+++ src/components/ListForm/ListForm.styles.js
@@ -2,19 +2,36 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     inputsContainer: {
-        borderColor: 'red',
-        borderWidth: 1,
         width: "100%",
         flexDirection: 'row',
         alignItems: 'center',
+        gap: 8,
     },
     button: {
         borderRadius: 10,
         backgroundColor: '#02d31e'
+    },
+    totalContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginVertical: 20
+    },
+    totalText: {
+        fontSize: 24
+    },
+    btnDeleteList: {
+        backgroundColor: 'red',
+        borderRadius: 10
+    },
+    confirmModal: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+    },
+    deleteBtn: {
+        backgroundColor: 'red',
+        borderRadius: 10
+    }, 
+    cancelBtn: {
+        borderRadius: 10
     }
 })
-
-
-
-
-
