8a5ad17b44ff5a3ce56a236d0171c383e5a4066d
rockop11 <rockoponce1422@gmail.com>
2024-01-17 16:01:10 -0300

    added status bar, and app icon

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { AppNavigator } from './src/navigation/AppNavigation';
+import { StatusBar } from 'expo-status-bar';
 import Toast from 'react-native-toast-message';
-import { app } from "./src/utils/firebase"
 
 
 function App() {
@@ -9,9 +9,9 @@ function App() {
     <NavigationContainer>
       <AppNavigator />
       <Toast />
+      <StatusBar style="auto" backgroundColor='#fff' />
     </NavigationContainer>
   );
 }
 
 export default App;
-
